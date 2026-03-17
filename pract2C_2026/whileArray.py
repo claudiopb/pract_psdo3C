6b70e2539fc61596a90f9a5a2793e200a98f8886
claudiopb <hai_madre@hotmail.com>
2026-03-17 10:44:41 -0600

    Update whileArray.py

--- pract2C_2026/whileArray.py
+++ pract2C_2026/whileArray.py
@@ -1,4 +1,18 @@
 miarreglo = ['Jones','Jill','Lisa','Stan','Bob','Alice']
+print(miarreglo)
+print(" ")
 i = 0
-while len(miarreglo) <= 6:
-    
+while i < len(miarreglo):
+    print(miarreglo[i])
+    if miarreglo[i] == 'Lisa':
+        print(f'Encontre a {miarreglo[i]}')
+        print("haciendo cambio")
+        miarreglo[i] = 'Elisa'
+    elif miarreglo[i] == 'Bob':
+        print(f'Encontre a {miarreglo[i]}')
+        print("haciendo cambio")
+        miarreglo[i] = 'Esponja'
+    i += 1
+
+print(' ')
+print(miarreglo)    
