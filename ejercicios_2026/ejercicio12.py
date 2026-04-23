a8c9722f677d9c5b871d43bb9d8a47a662526bac
claudiopb <hai_madre@hotmail.com>
2026-04-23 12:41:49 -0600

    summary

--- ejercicios_2026/ejercicio12.py
+++ ejercicios_2026/ejercicio12.py
@@ -1,8 +1,9 @@
-        
 numbers_x = [10,20,30,40,10]
+numbers_y = [75,65,35,75,35]
 
-item = len(numbers_x)
-
-print(item)
-print(numbers_x[0])
-print(numbers_x[item - 1])
+for listas in [numbers_x, numbers_y]:
+    comparacion= len(listas)
+    if listas[0] == listas[comparacion - 1]:
+        print("Lista dada:",listas, "|" "el resultado es verdadero")
+    else:
+        print("Lista dada:",listas, "|" "el resultado es falso")
