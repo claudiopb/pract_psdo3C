74c6e77f93f550d86f57625485c40d5006813b75
claudiopb <hai_madre@hotmail.com>
2026-05-01 19:42:47 -0600

    Summary

---
+++ ejercicios_2026/ejercicio13.py
@@ -0,0 +1,6 @@
+num_list = [10, 20, 33, 46, 55]
+print("Divisible por 5:")
+
+for lista in num_list:
+    if lista % 5 == 0:
+        print(lista)
---
+++ ejercicios_2026/ejercicio6.py
@@ -0,0 +1,9 @@
+numero=5
+factorial=1
+
+for i in range(1, numero + 1): #(1, 2, 3, 4, 5)
+	factorial=factorial*i
+	
+	print(factorial)
+	
+print("El factorial del numero ", numero, " es: ", factorial)
